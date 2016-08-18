8b09e81d6c92b0085ef4705cf0ff66a75eba4c14
yangzhiw <yang993505105@gamil.com>
2016-08-18 11:12:53 +0800

    多数据jpa数据源配置

--- src/test/java/com/juzi/MutiJpaSourceTest.java
+++ src/test/java/com/juzi/MutiJpaSourceTest.java
@@ -1,7 +1,50 @@
 package com.juzi;
 
+import com.juzi.domain.p.User;
+import com.juzi.domain.p.UserRepository;
+import com.juzi.domain.s.Message;
+import com.juzi.domain.s.MessageRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * Created by yzw on 2016/8/18 0018.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
 public class MutiJpaSourceTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        userRepository.save(new User("aaa", 10));
+        userRepository.save(new User("bbb", 20));
+        userRepository.save(new User("ccc", 30));
+        userRepository.save(new User("ddd", 40));
+        userRepository.save(new User("eee", 50));
+
+//        Assert.assertEquals(5, userRepository.findAll().size());
+
+        messageRepository.save(new Message("o1", "aaaaaaaaaa"));
+        messageRepository.save(new Message("o2", "bbbbbbbbbb"));
+        messageRepository.save(new Message("o3", "cccccccccc"));
+
+    //    Assert.assertEquals(3, messageRepository.findAll().size());
+
+    }
+
 }
